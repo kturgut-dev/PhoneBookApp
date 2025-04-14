c36e0005e7f9c64cc39867a74c79489ae675b551
Kürşat Turgut <mail@kursatturgut.com.tr>
2025-04-14 20:46:46 +0300

    bugs fixed

---
+++ shared/PhoneBookApp.Shared.Infrastructure/Extensions/DbContextMigrationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PhoneBookApp.Shared.Infrastructure.Extensions
+{
+    public static class DbContextMigrationExtensions
+    {
+        public static async Task EnsureMigrationAsync<TContext>(this IServiceProvider serviceProvider)
+            where TContext : DbContext
+        {
+            using IServiceScope scope = serviceProvider.CreateScope();
+            TContext context = scope.ServiceProvider.GetRequiredService<TContext>();
+
+            bool databaseExists = await context.Database.CanConnectAsync();
+
+            if (!databaseExists)
+            {
+                await context.Database.EnsureCreatedAsync();
+            }
+        }
+    }
+}
